541095ce5f76ef3d601a098cfc56d6d3073fae3a
winter-x64 <itswinterx64@gmail.com>
2026-06-22 01:29:52 +0530

    chore: bump version to 0.4.4 and name arduino zip dynamically

--- src/usmp_api.h
+++ src/usmp_api.h
@@ -13,10 +13,10 @@ extern "C" {
 // Version ───────────────────────────────────────────────────────────────────
 #define USMP_VERSION_MAJOR 0
 #define USMP_VERSION_MINOR 4
-#define USMP_VERSION_PATCH 3
+#define USMP_VERSION_PATCH 4
 
 /**
- * Get the library version string at runtime (e.g. "0.4.3").
+ * Get the library version string at runtime (e.g. "0.4.4").
  */
 const char* usmp_get_version(void);
 
